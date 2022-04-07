b6b92f7f278f19548e19cc48d0e45408461d8373
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2022-04-07 09:15:49 +0200

    Auto-diag snippet

--- site/templates/checklist.php
+++ site/templates/checklist.php
@@ -16,35 +16,7 @@
 					<button class="fr-accordion__btn" aria-expanded="false" aria-controls="accordion-<?= $key ?>"><?= $item->id() ?></button>
 				</h3>
 				<div class="fr-collapse" id="accordion-<?= $key ?>">
-
-					<h4 class="fr-h5">Pourquoi ?</h4>
-					<?= $item->desc()->kt() ?>
-
-					<h4 class="fr-h5 fr-mt-4w">Pour qui ?</h4>
-					<?= $item->who()->kt() ?>
-
-					<h4 class="fr-h5 fr-mt-4w">Test <?= $item->test() ?></h4>
-					<h5 class="fr-h6">Ce qu'il faut faire</h5>
-					<ol>
-					<?php foreach ($item->steps()->yaml() as $step): ?>
-							<li><?= $step ?></li>
-					<?php endforeach ?>
-					</ol>
-
-					<h5 class="fr-h6">Ce qu'il faut vérifier</h5>
-					<div class="fr-form-group">
-					    <fieldset class="fr-fieldset">
-				        <div class="fr-fieldset__content">
-					        <?php foreach ($item->checks()->yaml() as $check): ?>
-				        	<div class="fr-checkbox-group">
-				        		<input type="checkbox" id="checkboxes-1" name="checkboxes-1">
-            				<label class="fr-label" for="checkboxes-1"><?= $check ?>
-            				</label>
-        					</div>
-									<?php endforeach ?>
-				        </div>
-					    </fieldset>
-					</div>
+					<?php snippet('auto-diag', ['item' => $item]) ?>					
 				</div>
 			</section>
 			<?php endforeach ?>
