4aa9cf037c70382f674721dd8b4d6f8db9c3d797
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2022-04-07 09:12:00 +0200

    Auto-diag

--- site/templates/checklist.php
+++ site/templates/checklist.php
@@ -2,6 +2,7 @@
 
 <main id="content" role="main">
 	<div class="fr-container fr-container--fluid fr-my-8w">
+		<div class="fr-px-2w">
 
 		<h1><?= $page->title() ?></h1>
 		<p class="fr-text--lead"><?= $page->desc() ?></p>
@@ -12,53 +13,46 @@
 			<?php foreach ($page->items()->toStructure() as $key => $item): ?>
 			<section class="fr-accordion">
 				<h3 class="fr-accordion__title">
-					<button class="fr-accordion__btn" aria-expanded="false" aria-controls="accordion-<?= $key ?>"><?= $item->title() ?></button>
+					<button class="fr-accordion__btn" aria-expanded="false" aria-controls="accordion-<?= $key ?>"><?= $item->id() ?></button>
 				</h3>
 				<div class="fr-collapse" id="accordion-<?= $key ?>">
-					<p class="fr-text--regular fr-text--alt"><?= $item->desc() ?></p>
 
-					<div class="fr-highlight">
-						<p class="fr-text--sm"><strong>Pour qui ?</strong> <?= $item->who() ?></p>
-					</div>
+					<h4 class="fr-h5">Pourquoi ?</h4>
+					<?= $item->desc()->kt() ?>
 
-					<h4>Méthodologie</h4>
-					TODO : faire une checklist de trucs à cocher
+					<h4 class="fr-h5 fr-mt-4w">Pour qui ?</h4>
+					<?= $item->who()->kt() ?>
+
+					<h4 class="fr-h5 fr-mt-4w">Test <?= $item->test() ?></h4>
+					<h5 class="fr-h6">Ce qu'il faut faire</h5>
+					<ol>
+					<?php foreach ($item->steps()->yaml() as $step): ?>
+							<li><?= $step ?></li>
+					<?php endforeach ?>
+					</ol>
+
+					<h5 class="fr-h6">Ce qu'il faut vérifier</h5>
 					<div class="fr-form-group">
 					    <fieldset class="fr-fieldset">
-					        <legend class="fr-fieldset__legend fr-text--regular" id='checkboxes-legend'>
-					            Légende pour l’ensemble de champs
-					        </legend>
-					        <div class="fr-fieldset__content">
-					            <div class="fr-checkbox-group">
-					                <input type="checkbox" id="checkboxes-1" name="checkboxes-1">
-					                <label class="fr-label" for="checkboxes-1">
-					                    Label checkbox
-					                </label>
-					            </div>
-					            <div class="fr-checkbox-group">
-					                <input checked type="checkbox" id="checkboxes-2" name="checkboxes-2">
-					                <label class="fr-label" for="checkboxes-2">
-					                    Label checkbox
-					                </label>
-					            </div>
-					            <div class="fr-checkbox-group">
-					                <input type="checkbox" id="checkboxes-3" name="checkboxes-3">
-					                <label class="fr-label" for="checkboxes-3">
-					                    Label checkbox
-					                </label>
-					            </div>
-					        </div>
+				        <div class="fr-fieldset__content">
+					        <?php foreach ($item->checks()->yaml() as $check): ?>
+				        	<div class="fr-checkbox-group">
+				        		<input type="checkbox" id="checkboxes-1" name="checkboxes-1">
+            				<label class="fr-label" for="checkboxes-1"><?= $check ?>
+            				</label>
+        					</div>
+									<?php endforeach ?>
+				        </div>
 					    </fieldset>
 					</div>
-					<?= $item->methodo()->kt() ?>
-
-					<h4>Vérifications</h4>
-					<?= $item->verification()->kt() ?>
 				</div>
 			</section>
 			<?php endforeach ?>
 		</div>
 
+		<?= $page->conclusion()->kt() ?>
+
+	</div>
 	</div>
 </main>
 
