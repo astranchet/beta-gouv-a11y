9e1b251ed6bdbf0e3b0e4d9aac1ac437544d947c
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2022-04-07 10:16:09 +0200

    Audit flash page

---
+++ site/templates/accordions.php
@@ -0,0 +1,31 @@
+<?php snippet('dsfr/header') ?>
+
+<main id="content" role="main">
+	<div class="fr-container fr-container--fluid fr-my-8w">
+		<div class="fr-px-2w">
+
+		<h1><?= $page->title() ?></h1>
+		<p class="fr-text--lead"><?= $page->desc() ?></p>
+
+			
+		<?= $page->intro()->kt() ?>
+
+		<div class="fr-accordions-group">
+			<?php foreach ($page->items()->toStructure() as $key => $item): ?>
+			<section class="fr-accordion">
+				<h3 class="fr-accordion__title">
+					<button class="fr-accordion__btn" aria-expanded="false" aria-controls="accordion-<?= $key ?>"><?= $item->title() ?></button>
+				</h3>
+				<div class="fr-collapse" id="accordion-<?= $key ?>">
+					<?php snippet('auto-flash', ['item' => $item]) ?>					
+				</div>
+			</section>
+			<?php endforeach ?>
+		</div>
+
+		<?= $page->conclusion()->kt() ?>
+	</div>
+	</div>
+</main>
+
+<?php snippet('dsfr/footer') ?>
